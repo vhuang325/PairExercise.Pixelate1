df66984f4894ecbdf8ecd1b7d532054f30360f68
vhuang325 <vhuang325@gmail.com>
2023-06-02 13:08:12 -0400

    added first tab

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Your code here
+const table = document.querySelector(`table`);
+const addRowButton = document.querySelector(`#add-row`)
+
+function makeRows(){
+   const row = document.createElement(`tr`);
+   for(i = 0; i < 20; i++){
+    const dataCells = document.createElement(`td`);
+    row.appendChild(dataCells);
+   };
+   table.appendChild(row);
+};
+
+addRowButton.addEventListener(`click`, makeRows);
+
+// hi
