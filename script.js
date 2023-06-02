464ed89e6fc3791def48b0d722203cdac024419d
vhuang325 <vhuang325@gmail.com>
2023-06-02 15:42:02 -0400

    did some of pickColor

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Your code here
 const table = document.querySelector(`table`);
 const addRowButton = document.querySelector(`#add-row`);
+const select = document.querySelector(`select`);
+let chosenColor = `green`;
 
 function makeRows() {
   const row = document.createElement(`tr`);
@@ -14,12 +16,26 @@ function makeRows() {
 const colorize = function (event) {
   const target = event.target;
   if (target.className.length) {
-    target.className = "";
+    target.className = ``;
   } else {
-    target.className = "red";
+    target.className = chosenColor;
   }
 };
+
+function pickColor(target){
+ if(target.className = `red`){
+   chosenColor = `red`;
+ } else if (target.className = `blue`){
+   chosenColor = `blue`;
+ } else if (target.className = `yellow`){
+   chosenColor = `yellow`;
+ } else if (target.className = `green`){
+   chosenColor = `green`;
+ }
+}
 
 addRowButton.addEventListener(`click`, makeRows);
 
 table.addEventListener(`click`, colorize);
+
+select.addEventListener(`change`, pickColor);
