6f8d36857fd28864248efa03e96cc0b8f099177f
Fulongzhou <zhoufulong89@gmail.com>
2023-06-02 15:09:00 -0400

    add second tab

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 // Your code here
 const table = document.querySelector(`table`);
-const addRowButton = document.querySelector(`#add-row`)
+const addRowButton = document.querySelector(`#add-row`);
 
-function makeRows(){
-   const row = document.createElement(`tr`);
-   for(i = 0; i < 20; i++){
+function makeRows() {
+  const row = document.createElement(`tr`);
+  for (i = 0; i < 20; i++) {
     const dataCells = document.createElement(`td`);
     row.appendChild(dataCells);
-   };
-   table.appendChild(row);
+  }
+  table.appendChild(row);
+}
+
+const colorize = function (event) {
+  const target = event.target;
+  if (target.className.length) {
+    target.className = "";
+  } else {
+    target.className = "red";
+  }
 };
 
 addRowButton.addEventListener(`click`, makeRows);
 
-// hi
+table.addEventListener(`click`, colorize);
